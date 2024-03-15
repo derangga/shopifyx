8edf22eef13ea5c952b8303de7148f1b4e7f305b
zein <m.abdul@gooddoctor.co.id>
2024-03-16 00:37:34 +0700

    feat(metrics): prometheus integration

--- go.mod
+++ go.mod
@@ -11,6 +11,8 @@ require (
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
@@ -19,12 +21,16 @@ require (
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/common v0.48.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
+	google.golang.org/protobuf v1.32.0 // indirect
 )
 
 require (
@@ -37,6 +43,7 @@ require (
 	github.com/labstack/echo/v4 v4.11.4
 	github.com/lib/pq v1.10.9
 	github.com/pkg/errors v0.9.1
+	github.com/prometheus/client_golang v1.19.0
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
--- internal/http/routes.go
+++ internal/http/routes.go
@@ -1,40 +1,74 @@
 package http
 
 import (
+	"net/http"
+	"strconv"
+	"time"
+
 	"github.com/derangga/shopifyx/internal/http/handler"
 	"github.com/derangga/shopifyx/internal/http/middleware"
 	"github.com/labstack/echo/v4"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+var reqHistogram *prometheus.HistogramVec
 
 func RegisterRoute(
 	e *echo.Echo,
 	h *handler.Handlers,
 	jwtAuth *middleware.JWTAuth,
+	rh *prometheus.HistogramVec,
 ) {
 	authMiddleware := jwtAuth.ToMiddleware()
+	reqHistogram = rh
 
+	// prometheus metrics
+	e.GET("/metrics", echo.WrapHandler(promhttp.Handler()))
+
+	// v1 route grouping
 	v1 := e.Group("/v1")
 
 	// user management
-	v1.POST("/user/register", h.AuthHandler.Register)
-	v1.POST("/user/login", h.AuthHandler.Login)
+	newRoute(v1, http.MethodPost, "/user/register", h.AuthHandler.Register)
+	newRoute(v1, http.MethodPost, "/user/login", h.AuthHandler.Login)
 
 	// bank account
-	v1.GET("/bank/account", h.BankHandler.Fetch, authMiddleware)
-	v1.POST("/bank/account", h.BankHandler.Create, authMiddleware)
-	v1.PATCH("/bank/account/:bankAccountId", h.BankHandler.Update, authMiddleware)
-	v1.DELETE("/bank/account/:bankAccountId", h.BankHandler.Delete, authMiddleware)
+	newRoute(v1, http.MethodGet, "/bank/account", h.BankHandler.Fetch, authMiddleware)
+	newRoute(v1, http.MethodPost, "/bank/account", h.BankHandler.Create, authMiddleware)
+	newRoute(v1, http.MethodPatch, "/bank/account/:bankAccountId", h.BankHandler.Update, authMiddleware)
+	newRoute(v1, http.MethodDelete, "/bank/account/:bankAccountId", h.BankHandler.Delete, authMiddleware)
 
-	// product management
-	v1.POST("/product", h.ProductHandler.Create, authMiddleware)
-	v1.PATCH("/product/:id", h.ProductHandler.Update, authMiddleware)
-	v1.DELETE("/product/:id", h.ProductHandler.Delete, authMiddleware)
-	v1.POST("/product/:id/stock", h.ProductHandler.UpdateStock, authMiddleware)
+	// product
+	newRoute(v1, http.MethodGet, "/product", h.ProductHandler.Fetch, authMiddleware)
+	newRoute(v1, http.MethodPost, "/product", h.ProductHandler.Create, authMiddleware)
+	newRoute(v1, http.MethodPatch, "/product/:id", h.ProductHandler.Update, authMiddleware)
+	newRoute(v1, http.MethodDelete, "/product/:id", h.ProductHandler.Delete, authMiddleware)
+	newRoute(v1, http.MethodPost, "/product/:id/stock", h.ProductHandler.UpdateStock, authMiddleware)
 
 	// image upload
-	v1.POST("/image", h.ImageHandler.Upload, authMiddleware)
-  // product page
-	v1.GET("/product", h.ProductHandler.Fetch, authMiddleware)
-  // payment
-	v1.POST("/product/:id/buy", h.PaymentHandler.Create, authMiddleware)
+	newRoute(v1, http.MethodPost, "/image", h.ImageHandler.Upload, authMiddleware)
+
+	// payment
+	newRoute(v1, http.MethodPost, "/product/:id/buy", h.PaymentHandler.Create, authMiddleware)
+}
+
+func newRoute(e *echo.Group, method string, path string, h echo.HandlerFunc, m ...echo.MiddlewareFunc) {
+	e.Add(method, path, wrapHandlerWithMetrics(path, method, h), m...)
+}
+
+func wrapHandlerWithMetrics(path string, method string, handler echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		startTime := time.Now()
+
+		// Execute the actual handler and catch any errors
+		err := handler(c)
+
+		// Regardless of whether an error occurred, record the metrics
+		duration := time.Since(startTime).Seconds()
+
+		reqHistogram.WithLabelValues(path, method, strconv.Itoa(c.Response().Status)).Observe(duration)
+
+		return err
+	}
 }
