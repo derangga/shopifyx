0ee572260fbb9e41a963fe87d844856273ee442d
bimaswara <bimaswara@deepdivestudios.gg>
2024-03-16 13:44:06 +0700

    fix (payment) : add purchase count

--- internal/repository/query/product.go
+++ internal/repository/query/product.go
@@ -7,6 +7,7 @@ const (
 	ProductGetByID = `SELECT id, user_id, name, price, image_url, stock, condition, tags, is_purchaseable, purchase_count, created_at, updated_at FROM product WHERE "id" = $1 AND "deleted_at" IS NULL`
 	ProductUpdate  = `UPDATE product set name=$3, price=$4, image_url=$5, condition=$6, tags=$7, is_purchaseable=$8, updated_at=$9
 		WHERE id=$1 and user_id=$2 and (deleted_at is null)`
-	ProductDelete      = `UPDATE product set deleted_at=$3 WHERE id=$1 and user_id=$2`
-	ProductStockUpdate = `UPDATE product set stock=$3 WHERE id=$1 and user_id=$2 and (deleted_at is null)`
+	ProductDelete              = `UPDATE product set deleted_at=$3 WHERE id=$1 and user_id=$2`
+	ProductStockUpdate         = `UPDATE product set stock=$3 WHERE id=$1 and user_id=$2 and (deleted_at is null)`
+	ProductPurchaseCountUpdate = `UPDATE product set purchase_count=$3 WHERE id=$1 and user_id=$2 and (deleted_at is null)`
 )
